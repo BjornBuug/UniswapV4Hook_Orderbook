340756ba5551e5af5669517ab4cd8d50b1c1f2bc
OuailT <tayarthouail@gmail.com>
2024-09-15 12:12:44 +0200

    Ogranizr and Clears comments

--- src/OrderBookHookV4.sol
+++ src/OrderBookHookV4.sol
@@ -1,46 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-// Parent contract to create a hook contract
 import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
-import {ERC20} from "solmate/src/tokens/ERC20.sol";
-
-// This library allows for transferring and holding native tokens and ERC20 tokens
-import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
-
-// Contains a struct to create Pool and their generating a poolId using PoolIdLibrary
-import {PoolKey} from "v4-core/src/types/PoolKey.sol";
-
-// Contains hooks
-import {Hooks} from "v4-core/src/libraries/Hooks.sol";
-
-// BalanceDelta Library for getting the amount0 and amount1 deltas from the BalanceDelta type
-import {BalanceDeltaLibrary, BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
-
-/// Library for getting the specified and unspecified deltas from the BeforeSwapDelta type()
-// Uesed to handle information about token amount changes before a swap happens
-import {BeforeSwapDeltaLibrary, BeforeSwapDelta} from "v4-core/src/types/BeforeSwapDelta.sol";
-
-// Interface to interact with a pool manager
-import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
-
-// engine that match orders using the order book contract
 import {IEngine} from "@standardweb3/exchange/interfaces/IEngine.sol";
-
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
 import {IERC20} from "openzeppelin/contracts/token/erc20/IERC20.sol";
 
-// Declare the hook contracts
 contract OrderBookHookV4 is BaseHook {
-    using CurrencyLibrary for Currency;
-    using BalanceDeltaLibrary for BalanceDelta;
+    using PoolIdLibrary for PoolKey;
     using BeforeSwapDeltaLibrary for BeforeSwapDelta;
+    using CurrencyLibrary for Currency;
+
+    // NOTE: ---------------------------------------------------------
+    // state variables should typically be unique to a pool
+    // a single hook contract should be able to service multiple pools
+    // ---------------------------------------------------------------
+
+    mapping(PoolId => uint256 count) public beforeSwapCount;
+    mapping(PoolId => uint256 count) public afterSwapCount;
+
+    mapping(PoolId => uint256 count) public beforeAddLiquidityCount;
+    mapping(PoolId => uint256 count) public beforeRemoveLiquidityCount;
 
     address matchingEngine;
     address weth;
 
-    // Pseudo code
-    // include a constructor as well as basehook contract
-    // utilize the PoolManager contract, weth, matchingengine
     constructor(
         IPoolManager _poolmanager,
         address _matchingengine,
@@ -50,42 +41,31 @@ contract OrderBookHookV4 is BaseHook {
         weth = _weth;
     }
 
-    /* beforeSwapReturnDelta VS beforeSwap : Could be written as as post or tweet
-    @note The "beforeSwapReturnDelta" functionality is used in the "Committing Capital" phase.
-    It allows the system to dynamically adjust the amount being swapped in the AMM part of the transaction  
-    For example, if a user wants to swap 100 USDC for ETH, the hook might use beforeSwapReturnDelta to:
-    Reduce the swap amount to 80 USDC
-    Reserve the remaining 20 USDC for the orderbook part of the system */
-
-    // Override this function from based hook to specified which hooks will be used in this contract hooks.
-    // NOTE: we are going to use the FLAG associted with every permissions to deploy a hook contract
-    // These hooks Hooks lives in Hooks.sol
-    // NOTE: in my usecase I need to enable BeforeSwap and BeforeSwapReturnData
     function getHookPermissions()
         public
         pure
         override
         returns (Hooks.Permissions memory)
     {
-        Hooks.Permissions({
-            beforeInitialize: false,
-            afterInitialize: false,
-            beforeAddLiquidity: false,
-            beforeRemoveLiquidity: false,
-            afterAddLiquidity: false,
-            afterRemoveLiquidity: false,
-            beforeSwap: true, // It can perform checks or modifications but doesn't directly alter the swap amounts
-            afterSwap: false,
-            beforeDonate: false,
-            afterDonate: false,
-            beforeSwapReturnDelta: true, // It allows the hook to actually modify the amounts being swapped.
-            afterSwapReturnDelta: false,
-            afterAddLiquidityReturnDelta: false,
-            afterRemoveLiquidityReturnDelta: false
-        });
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                afterAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: true,
+                afterSwap: false,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: true, // true
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
     }
 
-    // Creates a BeforeSwapDelta from specified and unspecified
     function toBeforeSwapDelta(
         int128 deltaSpecified,
         int128 deltaUnspecified
@@ -102,60 +82,28 @@ contract OrderBookHookV4 is BaseHook {
         }
     }
 
-    // Before swap hook we can add any logic
+    // // TODO Modifier where only poolManager can call this beforeSwap.
     function beforeSwap(
         address,
         PoolKey calldata key,
         IPoolManager.SwapParams calldata swapParams,
-        bytes calldata hookData
-    )
-        external
-        override
-        returns (
-            // poolManagerOnly, // NOTE:add this later
-            bytes4,
-            BeforeSwapDelta,
-            uint24
-        )
-    {
-        // Get original deltas from the swap parameters
-        BeforeSwapDelta before = BeforeSwapDelta.wrap(
-            swapParams.amountSpecified
-        );
-
-        // Calculate how much should be used for the limit order.
-        uint128 amount = _limitOrder(key, swapParams, hookData);
-
-        // TODO: setup delta after taking input fund from pool manager and settle
-        // int128 afterOrder = before.getSpecifiedDelta() - int128(amount); // audit-issue uncomment this
-
-        // does the retur
-        // Return the function selector, new BeforeSwapDelta, and zero fees
+        bytes calldata orderHookData
+    ) external override returns (bytes4, BeforeSwapDelta, uint24) {
+        uint128 amount = _limitOrder(key, swapParams, orderHookData);
         return (
-            this.beforeSwap.selector,
-            toBeforeSwapDelta(int128(amount), 0),
+            BaseHook.beforeSwap.selector,
+            toBeforeSwapDelta(int128(amount), 0), // ?? check this back if it reverts
             0
         );
     }
 
-    // The swapParams to conduct a swap
-    /**
-     struct SwapParams {
-        bool zeroForOne;
-        int256 amountSpecified;
-        uint160 sqrtPriceLimitX96;
-    }
-    */
-
+    // function that allow the limit trade to be executed using orderHookData
     function _limitOrder(
-        PoolKey calldata key, // The pool key
+        PoolKey calldata key,
         IPoolManager.SwapParams calldata swapParams,
-        bytes calldata hookData // The hook data
+        bytes calldata orderHookData
     ) internal returns (uint128 amountDelta) {
-        if (hookData.length == 0) return 0;
-
-        // @audit-info "The user specifiy the amount of tokens to swapped
-        // using limit order?
+        if (orderHookData.length == 0) return 0;
 
         (
             uint256 limitPrice,
@@ -163,11 +111,12 @@ contract OrderBookHookV4 is BaseHook {
             address recipient,
             bool isMaker,
             uint32 n
-        ) = abi.decode(hookData, (uint256, uint256, address, bool, uint32));
+        ) = abi.decode(
+                orderHookData,
+                (uint256, uint256, address, bool, uint32)
+            );
 
-        // TODO: check if amount is bigger than delta, if it is, return delta
-        // Take tokens out of PM to our hook contract
-
+        // Tranfer 0.001 ETH deposited by user from the poolManager to HookContract
         _take(
             swapParams.zeroForOne ? key.currency0 : key.currency1,
             uint128(amount)
@@ -219,17 +168,20 @@ contract OrderBookHookV4 is BaseHook {
         address recipient
     ) internal returns (uint256 total) {
         if (zeroForOne) {
+            // Selling token0 for token1
             if (token0 == address(0)) {
+                // If token0 is ETH (address 0), use limitSellETH
                 IEngine(payable(matchingEngine)).limitSellETH{value: amount}(
-                    token1,
+                    token1, // Token to buy
                     limitPrice,
-                    isMaker,
-                    n,
+                    isMaker, // at limit price
+                    n, // The maximum number of orders to match in the orderbook
                     recipient
                 );
                 return amount;
             }
-            // else
+
+            // If token0 is not ETH, approve and use limitSell
             IERC20(token0).approve(matchingEngine, amount);
 
             (uint makePrice, uint placed, uint id) = IEngine(matchingEngine)
@@ -244,7 +196,9 @@ contract OrderBookHookV4 is BaseHook {
                 );
             return amount;
         } else {
+            // Buying token0 with token1
             if (token1 == address(0)) {
+                // If token1 is ETH (address 0), use limitBuyETH
                 IEngine(payable(matchingEngine)).limitBuyETH{value: amount}(
                     token0,
                     limitPrice,
@@ -268,10 +222,5 @@ contract OrderBookHookV4 is BaseHook {
         }
     }
 
-    receive() external payable {
-        // You can add any custom logic here if needed
-    }
-
-    // Before Swap Hooks
-    /// hookData Any data to pass to the callback, via `IUnlockCallback(msg.sender).unlockCallback(data)`
+    receive() external payable {}
 }
--- test/OrderBookHookV4.t.sol
+++ test/OrderBookHookV4.t.sol
@@ -1,56 +1,48 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
+import "forge-std/Test.sol";
 import {console2} from "forge-std/console2.sol";
-
-import {Deployers} from "v4-core/test/utils/Deployers.sol";
-import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
-
-import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
-
-import {WETH} from "solmate/src/tokens/WETH.sol";
-
-import {PoolManager} from "v4-core/src/PoolManager.sol";
-import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
-
-// This library allows for transferring and holding native tokens and ERC20 tokens
-import {Currency, CurrencyLibrary} from "v4-core/src/types/Currency.sol";
-
-// Contains hooks
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
-
-// To get SquareRoot price for a Swap.
 import {TickMath} from "v4-core/src/libraries/TickMath.sol";
-
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
+import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
 import {OrderBookHookV4} from "../src/OrderBookHookV4.sol";
-
-// HookMiner to allow for minitng/deploying new Hook address based on it Flag
-import {HookMiner} from "./utils/HookMiner.sol";
-
-// contract that match the limit order
+import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
+import {PositionConfig} from "v4-periphery/src/libraries/PositionConfig.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+import {WETH} from "solmate/src/tokens/WETH.sol";
 import {MatchingEngine} from "@standardweb3/exchange/MatchingEngine.sol";
-
 // Contract to create a orderbook factory
 import {OrderbookFactory} from "@standardweb3/exchange/orderbooks/OrderbookFactory.sol";
-
 import {Helpers} from "./utils/Helpers.sol";
+import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
+import {EasyPosm} from "./utils/EasyPosm.sol";
+import {Fixtures} from "./utils/Fixtures.sol";
 
-contract TestOrderBookHookV4 is Test, Deployers, Helpers {
+contract OrderBookHookV4Test is Test, Helpers, Fixtures {
+    using EasyPosm for IPositionManager;
+    using PoolIdLibrary for PoolKey;
     using CurrencyLibrary for Currency;
+    using StateLibrary for IPoolManager;
 
+    OrderBookHookV4 hook;
     MockERC20 UniToken;
-
-    // wrap the ETH as currency type
-
     Currency tokenCurrency;
-
-    // instance of the hook contract to test
-    OrderBookHookV4 orderbookHook;
 
     address public trader1;
     address public admin;
     address[] public users;
+
+    PoolId poolId;
+
+    uint256 tokenId;
+    PositionConfig config;
 
     function setUp() public {
         users = Helpers.createUsers(4);
@@ -72,19 +64,17 @@ contract TestOrderBookHookV4 is Test, Deployers, Helpers {
         // deploy matchineEngineFactory contract
         MatchingEngine matchingEngine = new MatchingEngine();
 
+        // Intilize the OrderBook factory
+        orderBookFactory.initialize(address(matchingEngine));
+
         // Initilize the matching Engine
         matchingEngine.initialize(
             address(orderBookFactory),
-            address(admin),
+            address(admin), // could be payable
             address(weth)
         );
 
-        // Initilize the orderbook
-        orderBookFactory.initialize(address(matchingEngine));
-        // --------------------------------------------------------------
-
-        // 2. Deploy PoolManager and RoutersContracts
-
+        // creates the pool manager, utility routers, and test tokens
         deployFreshManagerAndRouters();
 
         // Deploy ERC20 tokens
@@ -97,14 +87,11 @@ contract TestOrderBookHookV4 is Test, Deployers, Helpers {
         UniToken.mint(address(this), 1_000 ether);
         UniToken.mint(trader1, 1_0000 ether);
 
-        // --------------------------------------------------------------
-
-        // 3- Deploy the hook to an address with the correct Flag used inside the hook contract
-        // Combine flags using bitwise OR "|"
+        // Deploy the hook to an address with the correct flags
         address flags = address(
             uint160(
                 Hooks.BEFORE_SWAP_FLAG | Hooks.BEFORE_SWAP_RETURNS_DELTA_FLAG
-            )
+            ) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
         );
 
         bytes memory constructorArgs = abi.encode(
@@ -113,15 +100,12 @@ contract TestOrderBookHookV4 is Test, Deployers, Helpers {
             address(weth)
         );
 
-        // bytes memory constructorArgs = abi.encode(manager);
-
         deployCodeTo(
             "OrderBookHookV4.sol:OrderBookHookV4",
             constructorArgs,
             flags
         );
-
-        orderbookHook = OrderBookHookV4(payable(flags));
+        hook = OrderBookHookV4(payable(flags)); // @audit-info check payable if it revert
 
         // Approve Max amount of UniToken to be spend by swap router and modifyliquidity router
         // address(this) approval
@@ -136,25 +120,69 @@ contract TestOrderBookHookV4 is Test, Deployers, Helpers {
         UniToken.approve(address(matchingEngine), type(uint256).max);
         vm.stopPrank();
 
-        // // Initilize a pool
-        // (key, ) = initPool(
-        //     ethCurrency,
-        //     tokenCurrency,
-        //     orderbookHook,
-        //     3000,
-        //     SQRT_PRICE_1_1,
-        //     ZERO_BYTES
-        // );
+        // Initilize a pool
+        (key, ) = initPool(
+            ethCurrency,
+            tokenCurrency,
+            hook,
+            3000,
+            SQRT_PRICE_1_1,
+            ZERO_BYTES
+        );
 
-        // // add the above pair in the matching engine contract
-        // matchingEngine.addPair(
-        //     address(weth), // base
-        //     Currency.unwrap(tokenCurrency), // quote
-        //     2000e8 // 2000e8 // InitalMarket price were 1ETH = 2000 USDC
-        // );
+        // add the above pair in the matching engine contract
+        matchingEngine.addPair(
+            address(weth), // base
+            Currency.unwrap(tokenCurrency), // quote
+            2000e8 // 2000e8 // InitalMarket price were 1ETH = 2000 USDC
+        );
     }
 
-    function test_console() public {
-        console2.log("Hello there");
+    function test_addLiquidityAndSwap() public {
+        // Set no referrer in the hook data
+        bytes memory hookData = hook.getHookData(
+            2000e8, // => limitPrice in the order book were 1ETH = 2000 USDC
+            100000, // The amount of the quote tokens to get from the trade
+            trader1, // It can be the swaprouter, PoolManager, contractHook
+            true,
+            2
+        );
+
+        // getAmountsForLiquidity
+
+        // How we landed on 0.003 ether here is based on computing value of x and y given
+        // total value of delta L (liquidity delta) = 1 ether
+        // This is done by computing x and y from the equation shown in Ticks and Q64.96 Numbers lesson
+        // View the full code for this lesson on GitHub which has additional comments
+        // showing the exact computation and a Python script to do that calculation for you
+        modifyLiquidityRouter.modifyLiquidity{value: 0.003 ether}(
+            key,
+            IPoolManager.ModifyLiquidityParams({
+                tickLower: -60,
+                tickUpper: 60,
+                liquidityDelta: 1 ether, // we provide 1 as liquidity to the pool(key)
+                salt: 0
+            }),
+            hookData
+        );
+
+        // Now we swap
+        // We will swap 0.001 ether for tokens
+        // We should get 20% of 0.001 * 10**18 points
+        // = 2 * 10**14
+        vm.prank(trader1);
+        swapRouter.swap{value: 0.001 ether}(
+            key,
+            IPoolManager.SwapParams({
+                zeroForOne: true,
+                amountSpecified: -0.001 ether, // Exact input for output swap
+                sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
+            }),
+            PoolSwapTest.TestSettings({
+                takeClaims: true,
+                settleUsingBurn: false
+            }),
+            hookData
+        );
     }
 }
